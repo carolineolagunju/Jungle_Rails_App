6952bb94fa35f791402b0b20be8d3773702ff2d7
carolineolagunju <olamidejr21@gmail.com>
2023-10-28 23:08:48 -0600

    add authentication method for Missing Admin Security

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,14 @@
 class Admin::ProductsController < ApplicationController
 
+  before_action :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "Jungle" && password == "book"
+    end
+  end
+
+
   def index
     @products = Product.order(id: :desc).all
   end
