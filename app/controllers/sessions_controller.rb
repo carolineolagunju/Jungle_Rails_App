0d7b8a04d4dda88283ac5ad46881bc60faeb868e
carolineolagunju <olamidejr21@gmail.com>
2023-10-31 04:47:50 -0600

    refactored method create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      redirect_to root_path, notice: 'Logged in successfully!'
+    user = User.find_by_email(params[:email])
+  
+    if user
+      if user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to root_path, notice: 'Logged in successfully!'
+      else
+        flash.now[:alert] = 'Invalid email or password'
+        render :new
+      end
     else
-      flash.now[:alert] = 'Invalid email or password'
+      flash.now[:alert] = 'User not found'
       render :new
     end
   end
+  
 
   def destroy
     session[:user_id] = nil
