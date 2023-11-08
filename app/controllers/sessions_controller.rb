18279f44f71fd7bc2bde5e84c65d18895a775dd8
carolineolagunju <olamidejr21@gmail.com>
2023-11-07 22:57:40 -0700

    Displayed validation message to user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user
       if user.authenticate(params[:password])
         session[:user_id] = user.id
-        redirect_to root_path, notice: 'Logged in successfully!'
+        flash[:notice] = "Successfully logged in as #{user.first_name}"
       else
         flash.now[:alert] = 'Invalid email or password'
         render :new
@@ -18,10 +18,9 @@ class SessionsController < ApplicationController
       render :new
     end
   end
-  
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: 'Logged out successfully!'
+    flash[:notice] = 'Logged out successfully!'
   end
 end
